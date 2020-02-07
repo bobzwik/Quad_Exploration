1ca6d1850aecf1d2e288aa9a0e63a37d151b5d4e
John <john.bobzwik@gmail.com>
2020-02-06 22:36:51 -0500

    Update view only once per frame

--- Simulation/utils/animation.py
+++ Simulation/utils/animation.py
@@ -32,8 +32,47 @@ class ColorMarkers(visuals.Markers):
 
         self._data['a_bg_color'] = face_color
         self._vbo.set_data(self._data)
-        # self.shared_program.bind(self._vbo)
         self.update()
+
+class NonUpdatingTurntable(vispy.scene.cameras.TurntableCamera):
+    def __init__(self, **kwargs):
+        super(NonUpdatingTurntable, self).__init__(**kwargs)
+    
+    @property
+    def azimuth(self):
+        """ The angle of the camera in degrees around the y axis. An angle of
+        0 places the camera within the (y, z) plane.
+        """
+        return self._azimuth
+
+    @azimuth.setter
+    def azimuth(self, azim):
+        azim = float(azim)
+        while azim < -180:
+            azim += 360
+        while azim > 180:
+            azim -= 360
+        self._azimuth = azim
+        # self.view_changed()
+    
+    @property
+    def center(self):
+        """ The center location for this camera
+
+        The exact meaning of this value differs per type of camera, but
+        generally means the point of interest or the rotation point.
+        """
+        return self._center or (0, 0, 0)
+    
+    @center.setter
+    def center(self, val):
+        if len(val) == 2:
+            self._center = float(val[0]), float(val[1]), 0.0
+        elif len(val) == 3:
+            self._center = float(val[0]), float(val[1]), float(val[2])
+        else:
+            raise ValueError('Center must be a 2 or 3 element tuple')
+        # self.view_changed()
 
 
 def sameAxisAnimation(t_all, waypoints, pos_all, quat_all, euler_all, sDes_tr_all, Ts, params, xyzType, yawType, potfld, notInRange_all, inRange_all, inField_all, ifsave):
@@ -64,7 +103,7 @@ def sameAxisAnimation(t_all, waypoints, pos_all, quat_all, euler_all, sDes_tr_al
     
     # Add Camera
     view = canvas.central_widget.add_view()
-    view.camera = vispy.scene.cameras.TurntableCamera(distance=3, elevation=8, azimuth=(-90-psi_ini), fov=80)
+    view.camera = NonUpdatingTurntable(distance=3, elevation=8, azimuth=(-90-psi_ini), fov=80)
     
     # Add Grid
     # grid = scene.visuals.GridLines(color=(1,1,1), parent=view.scene)
@@ -151,6 +190,7 @@ def sameAxisAnimation(t_all, waypoints, pos_all, quat_all, euler_all, sDes_tr_al
             colors[np.where(inRange_all[idx_now,:])[0]] = np.array([0, 1, 0, 0.7])
             colors[np.where(inField_all[idx_now,:])[0]] = np.array([1, 0, 0, 0.7])
             scatter.set_color(face_color=colors)
+            view.camera.view_changed()
             canvas.app.process_events()
 
             idx_prev = idx_now
