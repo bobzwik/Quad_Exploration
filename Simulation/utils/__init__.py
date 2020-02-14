16c04090da5bd8c81699e581d7bf1fe86d3d70dc
John <john.bobzwik@gmail.com>
2020-02-13 21:43:25 -0500

    better BoxMarkers, and added it to the simulation animation
    # Conflicts:
    #	Simulation/utils/__init__.py
    #	Simulation/utils/animation_timer.py

--- Simulation/utils/__init__.py
+++ Simulation/utils/__init__.py
@@ -2,5 +2,6 @@ from .rotationConversion import *
 from .stateConversions import *
 from .mixer import *
 from .display import *
-from .animation import *
+from .animation_timer import *
 from .quaternionFunctions import *
+from .boxmarkers import *
