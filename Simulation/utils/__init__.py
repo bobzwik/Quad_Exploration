9481a08de43d730bdaba9d4e3138c36bea386474
John <john.bobzwik@gmail.com>
2020-02-15 10:58:56 -0500

    updated 2nd boxmarkers file

--- Simulation/utils/__init__.py
+++ Simulation/utils/__init__.py
@@ -3,5 +3,6 @@ from .stateConversions import *
 from .mixer import *
 from .display import *
 from .animation_timer_multiMarkers import *
+# from .animation_timer import *
 from .quaternionFunctions import *
 from .boxmarkers import *
